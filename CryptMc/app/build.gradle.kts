74ab8fd248aabbddce848468fc6874c198a67b31
dariuscade1102-crypto <2.90972013e+08+dariuscade1102-crypto@users.noreply.github.com>
2026-09-18 14:20:12 +0000

    Opt into experimental Material 3 APIs

--- CryptMc/app/build.gradle.kts
+++ CryptMc/app/build.gradle.kts
@@ -58,6 +58,7 @@ android {
     }
     kotlinOptions {
         jvmTarget = "17"
+        freeCompilerArgs += "-opt-in=androidx.compose.material3.ExperimentalMaterial3Api"
     }
     buildFeatures {
         compose = true
